33c3eca39b047d192482339256fd1504f2583476
迷住搜狗命令 <1586498944@qq.com>
2021-10-07 14:54:27 +0800

    任务二demo-弹性布局

--- src/view/demo2/index.vue
+++ src/view/demo2/index.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>任务二</div>
+  <div class="demo2">
+    <div class="input">
+      <el-input
+        placeholder="请输入内容"
+        prefix-icon="el-icon-search"
+        v-model="input">
+      </el-input>
+    </div>
+    <div class="logo">
+      <img src="../../assets/logo-1.jpg" alt="">
+    </div>
+    <div class="container">
+      <div class="item" v-for="item in navs" :key="item.index">
+        <img class="image" :src="item.url" alt="">
+        <div class="description">{{item.name}}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import url from '@/assets/1.png'
 export default {
-data() {
-return {
-
-}
-},
+  data() {
+    return {
+      input: '',
+      navs: [
+        {name: '全部商品', url, index: 0},
+        {name: '优惠券', url, index: 1},
+        {name: '积分商品', url, index: 2},
+        {name: '积分签到', url, index: 3},
+      ]
+    }
+  },
 created() {
 
 },
@@ -18,5 +42,37 @@ mounted() {
 }
 </script>
 <style scoped>
+.demo2 {
+  box-sizing: border-box;
+}
+.input {
+  box-sizing: border-box;
+  padding: 10px;
+}
+.logo {
+  height: 200px;
+}
+.logo img {
+  width: 100%;
+  height: 100%;
 
+}
+.container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.container .item {
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #C0C4CC;
+  box-sizing: border-box;
+  padding: 10px 0;
+}
+.container .item .image {
+  width: 50px;
+  height: 50px;
+}
 </style>
