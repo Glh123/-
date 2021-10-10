1e02097f2d33f3a07480f15cb9c209ec628936f7
迷住搜狗命令 <1586498944@qq.com>
2021-10-10 15:57:34 +0800

    feat: Header

---
+++ src/components/header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="header">
+      <i :class="icon.icon1"></i>
+      <div class="title">{{title}}</div>
+      <i :class="icon.icon2"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {type: String, default: () => '商品列表'},
+    icon: {
+      type: Object,
+      default: () => {
+        return {
+          icon1: 'el-icon-arrow-left', icon2: 'el-icon-search'
+        }
+      }
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  created() {
+
+  },
+  mounted() {
+
+  }
+}
+</script>
+<style scoped>
+.header {
+  height: 50px;
+  display: flex;
+  align-items: center;
+}
+.header .el-icon-arrow-left {
+  margin-left: 15px;
+  color: #409EFF;
+}
+.header .el-icon-search {
+  margin-right: 15px;
+  color: #409EFF;
+}
+.header .title {
+  flex: 1;
+  text-align: center;
+}
+</style>
